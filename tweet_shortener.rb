0d0a20c43ee3b108c5b1ba39d5c9930cab9daadf
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 03:44:56 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -45,7 +45,7 @@ end
   def shortened_tweet_truncator(tweet)
     if tweet.length > 140
       arr = tweet.split('')
-      (arr[0...140]).join('')
+      new_tweet = (arr[0...140]).join('')
     end 
   end 
 
