23d5a9b0de6060de97a2e2f8eb33720275a3d996
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 03:32:56 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -44,7 +44,7 @@ def selective_tweet_shortener()
   end
   
   def shortened_tweet_truncator(tweet)
-    tweet.chars.keep_if.with_index {|char, idx| idx < 140  }
+    puts (tweet.chars.keep_if.with_index {|char, idx| idx < 140 }).join('')
   end 
 
   # Question 4
